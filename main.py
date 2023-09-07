b077253dd3b040cfa82dacf427bd288a3cc3fa5a
UVRS <aldarizky36@gmail.com>
2023-09-07 15:50:26 +0700

    Belanja

---
+++ main.py
@@ -0,0 +1,38 @@
+"""
+# membuat tugas ( membeli susu dan telur )
+"""
+print("\n")
+print("hello world")
+print("My name is Alda Rizky Sefa\n")
+
+print('Ibu memberikan perintah, "beli 1 botol susu dan 6 butir telur"')
+print('Azlan berkata, "baik ibu, Mana uang nya ?"')
+print("Ibu memberikan uangnya")
+print("Azlan pergi ke toko"'\n')
+
+jumlah_susu = 320
+jumlah_telur = 575
+if jumlah_susu > 0:
+    print(f'Azlan mengecek susu,ternyata jumlah susu {jumlah_susu} botol')
+else:
+    print("azlan mengecek telur")
+
+harga_susu = 15.000
+if harga_susu <= 15.000:
+    print("harga sesuai, Azlan meletakan 1 botol susu di keranjang belanja"'\n')
+else:
+    print("harga tidak sesuai, Azlan kembali ke rumah")
+if jumlah_telur > 0:
+    print(f"Azlan mengecek telur,jumlah telur ada {jumlah_telur} butir")
+else:
+    print("Azlan kembali ke rumah")
+
+harga_telur = 25.000
+if harga_telur <= 25.000:
+    print("harga sesuai, Azlan meletakan 6 telur di keranjang belanja")
+else:
+    print("harga tidak sesuai, Azlan kembali ke rumah")
+print("Azlan ke kasir untuk membayar")
+print("Azlan kembali ke rumah")
+print('Azlan berkata, "Ibu ,ini 1 botol susu dan 6 telurnya"')
+print('Ibu menjawab, "Terimakasih anakku"')
