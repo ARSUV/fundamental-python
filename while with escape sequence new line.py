b8958f03288c70cbfd0df1073d9018c9acfd4c5b
UVRS <aldarizky36@gmail.com>
2023-10-14 12:33:43 +0700

    perulangan dengan barisbaru

---
+++ while with escape sequence new line.py
@@ -0,0 +1,24 @@
+count = 0
+while count < 5:
+    print("Nilai count:", count)
+    count += 1
+print('\n')
+
+# penambahan enter per 2 kali perulangan dengan if,==,'\n' dan ()
+
+nilai = 0
+while nilai < 5:
+    print('putaran nilai ke', nilai+1)
+    if nilai == 1:
+        print('\n')
+    if nilai == 3:
+        print('\n')
+    nilai += 1
+print()
+
+# ada enter di setiap putaran
+
+nilai = 0
+while nilai < 5:
+    print('putaran ke', nilai+1, '\n')
+    nilai += 1
