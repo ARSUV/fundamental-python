c71a21a9ea395e3bcc6f0cc9e22a39407bd1a0f9
UVRS <aldarizky36@gmail.com>
2023-10-16 19:22:55 +0700

    coba coba

---
+++ STRING.py
@@ -0,0 +1,14 @@
+TEXT1 = "Ma'lumat"
+TEXT2 = r"C:\ngapa, "
+TEXT3 = 5 * 'ha'
+
+print(TEXT2 + TEXT3)
+
+TEXT4 = 'pohon pisang'
+aku = TEXT4[2:8]
+print(aku)
+
+TEXT5 = 'bangku sekolah '
+aku1 = TEXT5[-5]
+aku2 = TEXT5[1:]
+print('b' + aku2+TEXT3, 'wkwkw')
